172fa49e09785ec11375466dd4705a581e22eef3
Noorealam786 <shaikhnoorealam76@gmail.com>
2026-04-12 09:08:14 -0700

    Update student sets for exams

--- exp6.py
+++ exp6.py
@@ -1,7 +1,7 @@
 # students in exams
-cet = {"Kartavya", "Bob"}
-jee = {"Tathagat", "Bob"}
-neet = {"Saad", "Eve"}
+cet = {"Shanawaz", "Bob"}
+jee = {"Noorealam", "Bob"}
+neet = {"Hafin", "Eve"}
 print("All students:", cet|jee|neet) #Union
 print("Students in all exam:", cet & jee & neet) # intersection
 print("Cet but not in jee:", cet - jee) #Difference
